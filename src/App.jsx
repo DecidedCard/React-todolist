fc26898f0b24852a315cf200bba790b9bf7d2d0d
DecidedCard <heajun35@gmail.com>
2024-01-19 18:46:51 +0900

    feat: 완료기능 및 취소기능 추가, 컴포넌트 분리.

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import "./App.css";
 import Header from "./components/Header";
+import Main from "./components/Main";
 
 function App() {
   const [contents, setContents] = useState([
@@ -12,68 +13,10 @@ function App() {
     },
   ]);
 
-  const deleteTodo = (id) => {
-    const newContents = contents.filter((todo) => {
-      return todo.id !== id;
-    });
-    setContents(newContents);
-  };
-
-  const doneBtn = (id) => {
-    const doneTodo = contents.filter((todo) => {
-      return todo.id === id;
-    });
-    const filterContents = contents.filter((item) => {
-      return item === doneTodo;
-    });
-    console.log(filterContents);
-    const check = { ...doneTodo, isDone: true };
-    // setContents([...contents, check]);
-    // console.log(contents);
-  };
   return (
     <div>
       <Header contents={contents} setContents={setContents} />
-      <main>
-        <section>
-          <h2>Working...🔥🔥</h2>
-          <ul className="card-list">
-            {contents.map((i) => {
-              if (i.isDone === false) {
-                return (
-                  <li key={i.id} className="todo-card">
-                    <h3>{i.title}</h3>
-                    <p>{i.content}</p>
-                    <p className="todo-btn">
-                      <button onClick={() => deleteTodo(i.id)}>삭제하기</button>
-                      <button onClick={() => doneBtn(i.id)}>완료</button>
-                      <p>{i.isDone}</p>
-                    </p>
-                  </li>
-                );
-              }
-            })}
-          </ul>
-          <h2>Done..!🎆</h2>
-          <ul className="card-list">
-            {contents.map((i) => {
-              if (i.isDone === true) {
-                return (
-                  <li key={i.id} className="todo-card">
-                    <h3>{i.title}</h3>
-                    <p>{i.content}</p>
-                    <p className="todo-btn">
-                      <button onClick={() => deleteTodo(i.id)}>삭제하기</button>
-                      <button onClick={() => doneBtn(i.id)}>완료</button>
-                      <p>{i.isDone}</p>
-                    </p>
-                  </li>
-                );
-              }
-            })}
-          </ul>
-        </section>
-      </main>
+      <Main contents={contents} setContents={setContents} />
       <div></div>
     </div>
   );
---
+++ src/components/Main.jsx
@@ -0,0 +1,77 @@
+import React from "react";
+
+function Main({ contents, setContents }) {
+  const deleteTodo = (id) => {
+    const newContents = contents.filter((todo) => {
+      return todo.id !== id;
+    });
+    setContents(newContents);
+  };
+
+  const doneBtn = (id) => {
+    const check = contents.map((obj) => {
+      if (id === obj.id) {
+        return { ...obj, isDone: true };
+      } else {
+        return { ...obj };
+      }
+    });
+    setContents(check);
+  };
+
+  const cancleBtn = (id) => {
+    const check = contents.map((obj) => {
+      if (id === obj.id) {
+        return { ...obj, isDone: false };
+      } else {
+        return { ...obj };
+      }
+    });
+    setContents(check);
+  };
+
+  return (
+    <main>
+      <section>
+        <h2>Working...🔥🔥</h2>
+        <ul className="card-list">
+          {contents.map((i) => {
+            if (i.isDone === false) {
+              return (
+                <li key={i.id} className="todo-card">
+                  <h3>{i.title}</h3>
+                  <p>{i.content}</p>
+                  <p className="todo-btn">
+                    <button onClick={() => deleteTodo(i.id)}>삭제하기</button>
+                    <button onClick={() => doneBtn(i.id)}>완료</button>
+                    <p>{i.isDone}</p>
+                  </p>
+                </li>
+              );
+            }
+          })}
+        </ul>
+        <h2>Done..!🎆</h2>
+        <ul className="card-list">
+          {contents.map((i) => {
+            if (i.isDone === true) {
+              return (
+                <li key={i.id} className="todo-card">
+                  <h3>{i.title}</h3>
+                  <p>{i.content}</p>
+                  <p className="todo-btn">
+                    <button onClick={() => deleteTodo(i.id)}>삭제하기</button>
+                    <button onClick={() => cancleBtn(i.id)}>취소</button>
+                    <p>{i.isDone}</p>
+                  </p>
+                </li>
+              );
+            }
+          })}
+        </ul>
+      </section>
+    </main>
+  );
+}
+
+export default Main;
